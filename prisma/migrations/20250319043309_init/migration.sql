cf702c3d6583a4912aeb308a7300c7d1c6052ddc
tejasteju12 <steja.1si21ad059@gmail.com>
2025-03-19 11:23:51 +0530

    add initial database schema and CRUD operations for students and professors

---
+++ prisma/migrations/20250319043309_init/migration.sql
@@ -0,0 +1,30 @@
+CREATE TABLE "Student" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "dateOfBirth" TIMESTAMP NOT NULL,
+    "aadharNumber" TEXT NOT NULL UNIQUE,
+    "proctorId" TEXT NOT NULL,
+    
+    CONSTRAINT "Student_pkey" PRIMARY KEY ("id")
+);
+
+CREATE TABLE "Professor" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "seniority" TEXT NOT NULL,
+    "aadharNumber" TEXT NOT NULL UNIQUE,
+    
+    CONSTRAINT "Professor_pkey" PRIMARY KEY ("id")
+);
+
+CREATE TABLE "LibraryMembership" (
+    "id" TEXT NOT NULL,
+    "studentId" TEXT NOT NULL UNIQUE,
+    "issueDate" TIMESTAMP NOT NULL,
+    "expiryDate" TIMESTAMP NOT NULL,
+
+    CONSTRAINT "LibraryMembership_pkey" PRIMARY KEY ("id"),
+    CONSTRAINT "LibraryMembership_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "Student" ("id") ON DELETE CASCADE
+);
+
+ALTER TABLE "Student" ADD CONSTRAINT "Student_proctorId_fkey" FOREIGN KEY ("proctorId") REFERENCES "Professor" ("id") ON DELETE CASCADE;
